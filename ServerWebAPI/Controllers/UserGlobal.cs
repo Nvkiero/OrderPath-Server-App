03954229335016b6315aff4a337c0c90a1244e8c
hieuminh367 <hieu.0901087706@gmail.com>
2025-12-14 10:20:32 +0700

    chức năng Đăng kí và set database

---
+++ ServerWebAPI/Controllers/UserGlobal.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerWebAPI.Controllers
+{
+    public class UserRegister
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string Fullname { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public DateTime Birth { get; set; }
+        public string Phone { get; set; } = string.Empty;
+        public int Age { get; set; }
+        public string Address { get; set; } = string.Empty;
+    }
+    public class User
+    {
+        [Key] // Khai báo khóa chính
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] // Tự động tăng
+        public int Id { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string Fullname { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public DateTime Birth { get; set; }
+        public string Phone { get; set; } = string.Empty;
+        public int Age { get; set; }
+        public string Address { get; set; } = string.Empty;
+    }
+}
---
+++ ServerWebAPI/DataBase/DBcontext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ServerWebAPI.Controllers;
+
+namespace ServerWebAPI.DataBase
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; } = null!;
+    }
+}
